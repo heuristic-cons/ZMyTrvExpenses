fae6a656aa2a6f869e1bfd0adaa8402cac007ddf
Alex <alexandros.kontopoulos@heuristic-cons.com>
2019-08-23 15:59:03 +0100

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.getronics.hr.ptrvzmytravelexpenses.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.getronics.hr.ptrvzmytravelexpenses.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
